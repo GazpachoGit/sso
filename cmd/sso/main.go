ce8b526aa5aee6453a4b414a76cacb87349e0927
egor <egtornikov@gmail.com>
2025-08-11 14:35:39 +0200

    add log config

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -2,18 +2,48 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/GazpachoGit/sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	//init config
 	cfg := config.MustLoad()
 	fmt.Printf("Loaded config: %+v\n", cfg)
+
 	//init log(slog)
+	log := setupLogger(cfg.Env)
+	log.Info("Starting application", slog.String("env", cfg.Env))
 
 	//init app
 
 	//run gRPC server
 
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelInfo,
+		}))
+	}
+	return log
 }
